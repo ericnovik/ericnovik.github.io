6723cb0afbf67ba31c884b7fe4174a7f27842de3
Eric Novik <eric.novik@gmail.com>
2024-03-05 15:10:52 -0500

    Finished preparing lecture 1 for spring 24

--- bayes-course/01-lecture/two-truths/two-truths.R
+++ bayes-course/01-lecture/two-truths/two-truths.R
@@ -1,5 +1,6 @@
 library(googlesheets4)
 library(ggplot2)
+library(glue)
 thm <-
   theme_minimal() + theme(
     panel.background = element_rect(fill = "#f0f1eb", color = "#f0f1eb"),
@@ -8,18 +9,20 @@ thm <-
   )
 theme_set(thm)
 
-# Read google sheets data into R
-data <- read_sheet('https://docs.google.com/spreadsheets/d/1oyCbed2BpQzHtvpA4DynTlPbwZpxOzOePNw9tas40qU/edit#gid=186692089')
+url <- 'https://docs.google.com/spreadsheets/d/1oyCbed2BpQzHtvpA4DynTlPbwZpxOzOePNw9tas40qU/edit#gid=186692089'
+data <- read_sheet(url)
 colnames(data) <- c('time', 'group', 'score', 'correct')
 data
+n <- nrow(data)
 data <- data |> dplyr::mutate(correct = ifelse(correct == "Yes", 1, 0),
                               score_jitter = score + runif(n, -0.3, 0.3))
 data
 p <- ggplot(aes(score_jitter, correct), data = data)
 p <- p + geom_point() + xlim(0, 10) + 
   xlab("Certainty score") + 
-  ylab("Correct guess?")
+  ylab("Pr(Correct guess)")
 p
+
 # Fit using glm
 f1 <- glm(correct ~ score,
           family = binomial(link = "logit"),
@@ -32,7 +35,15 @@ b_hat <- coef(f1)["score"]
 scores <- seq(0, 10, len = 30)
 fitted <- data.frame(logit = arm::invlogit(a_hat + b_hat * scores),
                      scores = scores)
-p + geom_line(aes(scores, logit), linewidth = 0.2, data = fitted)
+p + geom_line(aes(scores, logit), linewidth = 0.2, data = fitted) +
+  geom_text(x = 3, 
+            y = 0.6, 
+            label = glue('Pr(Y = 1) = invlogit({round(a_hat, 2)} + {round(b_hat, 2)}*x)'),
+            color = 'brown',
+            size = 5)
+
+cat("Probability of the correct guess ranges from", arm::invlogit(a_hat) 
+    |> round(2), "to", arm::invlogit(a_hat + b_hat * 10) |> round(2))
 
 # plot in a larger range
 from <- -10; to <- 20
@@ -41,5 +52,5 @@ fitted <- data.frame(logit = arm::invlogit(a_hat + b_hat * scores),
                      scores = scores)
 p <- ggplot(aes(score_jitter, correct), data = data)
 p <- p + geom_point() + xlim(from, to) + 
-  xlab("Certainty score") + ylab("Correct guess?")
+  xlab("Certainty score") + ylab("Pr(Correct guess)")
 p + geom_line(aes(scores, logit), linewidth = 0.2, data = fitted)
