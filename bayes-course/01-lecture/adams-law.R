6b59a237dd50766b8169d40f6bb3f0fff5bbd10d
Eric Novik <eric.novik@gmail.com>
2026-03-16 16:38:07 -0400

    Mods for Spring 2026

--- bayes-course/01-lecture/adams-law.R
+++ bayes-course/01-lecture/adams-law.R
@@ -1,16 +1,19 @@
 library(purrr)
+
 n <- 20
-class_size <- sample(n)
-mean_vec <- runif(n = n, 2.8, 3.2)
-N <- sum(class_size)
-W <- class_size / N
-G <- class_size |>
-  map(\(x) rnorm(n = x, mean = mean_vec, sd = 1))
+class_sizes <- sample(n)
+mean_gpa_vec <- runif(n = n, 2.8, 3.2)
+N <- sum(class_sizes)
+W <- class_sizes / N
+G <- class_sizes |>
+  map(\(x) rnorm(n = x, mean = mean_gpa_vec, sd = 1))
 
 # E(Y)
 EY <- unlist(G) |> mean()
 print(EY)
+
 # E(E(Y | X))
-gpaXprob <- map_dbl(G, mean) * W
+class_means <- map_dbl(G, mean) # can't just average those
+gpaXprob <- class_means * W
 EEYX <- sum(gpaXprob)
 print(EEYX)
